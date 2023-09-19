d30eaf08fb1cd150b4b21f90240b98d849b081e4
Ali Khan <alikhan37544@gmail.com>
2023-09-19 20:41:05 +0530

    Update estimation_v2.py

--- estimation_v2.py
+++ estimation_v2.py
@@ -2,7 +2,7 @@
 
 import RPi.GPIO as GPIO
 import time
-import math
+import math 
 
 # Define the GPIO pins for the ultrasonic sensors
 sensor_pins = [4, 17, 18]  # Example GPIO pins, adjust as needed
